4bfc55d9fdd44cd400d4422f98301debbb43274c
Beatriz <beatriz.louresmacuco@gmail.com>
2025-07-28 19:12:04 +0200

    first commit

---
+++ ShareTheMealApp/ShareTheMealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareTheMealAppApp.swift
+//  ShareTheMealApp
+//
+//  Created by Beatriz Loures Macuco on 22.07.25.
+//
+
+import SwiftUI
+
+@main
+struct ShareTheMealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShareTheMealApp/View/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  ShareTheMealApp
+//
+//  Created by Beatriz Loures Macuco on 22.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        MealProgramListView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
